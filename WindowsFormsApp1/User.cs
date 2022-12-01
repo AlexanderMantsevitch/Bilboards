d5232d05bf09ba2fec2e2c20844e997bf1069c96
Alex <rct.mancevic@bsu.by>
2022-12-01 20:31:53 +0300

    + class user

--- WindowsFormsApp1/User.cs
+++ WindowsFormsApp1/User.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    class Class1
+    class User
     {
+        private string nameUser = "Null";
+        private string surnameUser = "Null";
+
+        public string get_nameUser ()
+        {
+            return this.nameUser;
+
+        }
+
+        public string get_surnameUser()
+        {
+            return this.surnameUser;
+
+        }
+
+
+        public void set_nameUser(string userName )
+        {
+            this.nameUser = userName;
+
+        }
+
+        public void set_surnameUser(string userSurname)
+        {
+            this.surnameUser = userSurname;
+        }
     }
 }
