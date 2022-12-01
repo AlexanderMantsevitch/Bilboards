7c8a42a064518f3962d660e2a7af6416ff7616c4
Alex <rct.mancevic@bsu.by>
2022-12-01 17:49:41 +0300

    Authorization + connect sql

--- WindowsFormsApp1/Login.Designer.cs
+++ WindowsFormsApp1/Login.Designer.cs
@@ -29,17 +29,16 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.errorAuthorizathionLbl = new System.Windows.Forms.Label();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.PasswordtextBox = new System.Windows.Forms.TextBox();
+            this.LogintextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.AutorithationLabel = new System.Windows.Forms.Label();
+            this.SignInButton = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -47,7 +46,6 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -56,101 +54,98 @@
             this.panel1.Size = new System.Drawing.Size(404, 371);
             this.panel1.TabIndex = 0;
             // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.button1);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel4.Location = new System.Drawing.Point(0, 210);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(404, 161);
-            this.panel4.TabIndex = 7;
-            
-            // 
-            // button1
-            // 
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(173, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Войти";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Controls.Add(this.textBox2);
-            this.panel3.Controls.Add(this.textBox1);
-            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.SignInButton);
+            this.panel3.Controls.Add(this.errorAuthorizathionLbl);
+            this.panel3.Controls.Add(this.loginLabel);
+            this.panel3.Controls.Add(this.PasswordtextBox);
+            this.panel3.Controls.Add(this.LogintextBox);
+            this.panel3.Controls.Add(this.passwordLabel);
             this.panel3.Location = new System.Drawing.Point(0, 147);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(407, 71);
+            this.panel3.Size = new System.Drawing.Size(407, 161);
             this.panel3.TabIndex = 5;
-            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
-            // label2
+            // errorAuthorizathionLbl
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(94, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 19);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "login:";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.errorAuthorizathionLbl.AutoSize = true;
+            this.errorAuthorizathionLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.errorAuthorizathionLbl.Location = new System.Drawing.Point(138, 61);
+            this.errorAuthorizathionLbl.Name = "errorAuthorizathionLbl";
+            this.errorAuthorizathionLbl.Size = new System.Drawing.Size(0, 13);
+            this.errorAuthorizathionLbl.TabIndex = 1;
             // 
-            // textBox2
+            // loginLabel
             // 
-            this.textBox2.Location = new System.Drawing.Point(141, 37);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(169, 20);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.UseSystemPasswordChar = true;
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginLabel.Location = new System.Drawing.Point(94, 13);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(40, 19);
+            this.loginLabel.TabIndex = 1;
+            this.loginLabel.Text = "login:";
+            this.loginLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox1
+            // PasswordtextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(141, 11);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 20);
-            this.textBox1.TabIndex = 2;
+            this.PasswordtextBox.Location = new System.Drawing.Point(141, 37);
+            this.PasswordtextBox.Name = "PasswordtextBox";
+            this.PasswordtextBox.Size = new System.Drawing.Size(169, 20);
+            this.PasswordtextBox.TabIndex = 4;
+            this.PasswordtextBox.UseSystemPasswordChar = true;
             // 
-            // label3
+            // LogintextBox
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(63, 36);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 19);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "password:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LogintextBox.Location = new System.Drawing.Point(141, 11);
+            this.LogintextBox.Name = "LogintextBox";
+            this.LogintextBox.Size = new System.Drawing.Size(169, 20);
+            this.LogintextBox.TabIndex = 2;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordLabel.Location = new System.Drawing.Point(63, 36);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(71, 19);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "password:";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.AutorithationLabel);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(404, 141);
             this.panel2.TabIndex = 0;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
-            // label1
+            // AutorithationLabel
             // 
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(404, 141);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Авторизация";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.AutorithationLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AutorithationLabel.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AutorithationLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.AutorithationLabel.Location = new System.Drawing.Point(0, 0);
+            this.AutorithationLabel.Name = "AutorithationLabel";
+            this.AutorithationLabel.Size = new System.Drawing.Size(404, 141);
+            this.AutorithationLabel.TabIndex = 0;
+            this.AutorithationLabel.Text = "Авторизация";
+            this.AutorithationLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.SignInButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.SignInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SignInButton.Location = new System.Drawing.Point(182, 79);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(75, 23);
+            this.SignInButton.TabIndex = 6;
+            this.SignInButton.Text = "Войти";
+            this.SignInButton.UseVisualStyleBackColor = true;
+            this.SignInButton.Click += new System.EventHandler(this.SignInButton_Click);
             // 
             // Login
             // 
@@ -163,9 +158,7 @@
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
-            this.Load += new System.EventHandler(this.Login_Load);
             this.panel1.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -177,13 +170,13 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label AutorithationLabel;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.TextBox LogintextBox;
+        private System.Windows.Forms.TextBox PasswordtextBox;
+        private System.Windows.Forms.Label passwordLabel;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label errorAuthorizathionLbl;
+        private System.Windows.Forms.Button SignInButton;
     }
 }
--- WindowsFormsApp1/Login.cs
+++ WindowsFormsApp1/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +18,42 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-      
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            String loginUser = LogintextBox.Text;
+            String passUser = PasswordtextBox.Text;
+
+            DataBase db = new DataBase();
+            DataTable dataTable = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand autorizathionSqlCommand = new MySqlCommand(" SELECT * FROM `users` WHERE `login` = @uL AND `password` = @uP ", db.getConnection());
+            autorizathionSqlCommand.Parameters.Add("@uL" , MySqlDbType.VarChar ).Value = loginUser;
+            autorizathionSqlCommand.Parameters.Add("@uP", MySqlDbType.VarChar).Value = passUser;
+
+            adapter.SelectCommand = autorizathionSqlCommand;
+            adapter.Fill(dataTable);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                foreach (DataColumn dc in dataTable.Columns)
+                {
+
+                    Console.Write("\t{0}", dc.ColumnName);
+
+                }
+                if (dataTable.Columns[2].Expression == "Admin")
+                {
+                   
+
+                }
+
+
+            }
+
+            else errorAuthorizathionLbl.Text = "Неправильный логин или пароль";
+        }
+
+  
     }
 }
